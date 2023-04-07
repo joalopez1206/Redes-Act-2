f8ec664f58f9fa70b7486c11e43de4fe34a2fa42
joalopez1206 <joalopez@ug.uchile.cl>
2023-04-06 22:01:40 -0400

    added tests and parser

--- src/http_parser.py
+++ src/http_parser.py
@@ -1,6 +1,32 @@
-def parse_http(msg):
-    return None
+HEAD = "head"
+BODY = "body"
+METHOD = "method"
+REQUEST_SEQUENCES = ("GET", "POST", "HEAD")
+RESPONSE_SEQUENCE = "HTTP"
+END_HEAD_SEQUENCE = '\r\n\r\n'
 
 
-def to_http(parsed_msg):
-    return None
+def parse_http(msg: str):
+    head, body = msg.split(END_HEAD_SEQUENCE)
+    return {HEAD: parse_head(head), BODY: parse_body(body)}
+
+
+def parse_head(head: str):
+    retdict = dict()
+    # Primero separamos el head en \r\n
+    lines = head.split("\r\n")
+    # Extraemos el metodo
+    retdict[METHOD] = lines[0]
+    # y luego recorremos todos los atributos
+    for line in lines[1:]:
+        line = line.split(":", maxsplit=1)
+        retdict[line[0]] = line[1]
+    return retdict
+
+
+def parse_body(body) -> str:
+    return body
+
+
+def to_http(parsed_msg: dict) -> str:
+    ...
---
+++ test/test_http_parser.py
@@ -0,0 +1,25 @@
+import unittest
+import src.http_parser as htp
+
+testmsg = """GET / undefined
+Host: localhost:8000
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3
+Accept-Encoding: gzip, deflate, br
+Connection: keep-alive
+Upgrade-Insecure-Requests: 1
+Sec-Fetch-Dest: document
+Sec-Fetch-Mode: navigate
+Sec-Fetch-Site: none
+Sec-Fetch-User: ?1""".replace("\n", "\r\n") + htp.END_HEAD_SEQUENCE
+
+
+class MyTestCase(unittest.TestCase):
+    def test_xd(self):
+        print(htp.parse_http(testmsg)[htp.HEAD])
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
