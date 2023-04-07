1ed641f00d2b932029e962c8dece7a15e468e368
joalopez1206 <joalopez@ug.uchile.cl>
2023-04-07 17:13:26 -0400

    Added messages for debugging and server response

--- src/http_server.py
+++ src/http_server.py
@@ -1,7 +1,8 @@
 # lets initialize a sever in port 8000
 from utils import *
 import http_parser as hpar
-
+import pprint
+pprinter = pprint.PrettyPrinter(indent=4, sort_dicts=False)
 ADDRESS = ('localhost', 8000)
 httpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Initializing server")
@@ -15,17 +16,21 @@ while True:
     # 4- y de ahí por el momento solo printeo.
     print("Waiting for clients")
     receiverSocket, addr = httpServer.accept()
-    print("A Message has been recived")
-    msg = recive_full_msg(receiverSocket, BUFFSIZE)
-    print("parsing message")
-    parsed_msg = hpar.parse_http(msg)
-    print("The http meesage is a:", parsed_msg["type"])
-    print(parsed_msg["message"])
-    print("translating back to http")
-    other_msg = hpar.to_http(parsed_msg)
-    if msg == other_msg:
-        print("nais")
-    else:
-        print("na")
-    receiverSocket.send(msg)
+    print("A Message has been recived, establishing communication channel")
+    msg = receiverSocket.recv(BUFFSIZE)
+    print("parsing message...")
+    parsed_msg = hpar.parse_http(msg.decode())
+    print("The http meesage is a:", hpar.isResponseOrRequest(parsed_msg))
+    pprinter.pprint(parsed_msg)
+    print("Answering The request:")
+    content_type = "text/html"
+    with open("hello-world.html") as fd:
+        str_to_append = fd.read()
+    cabezera = {hpar.METHOD:"HTTP/1.1 200 OK", 'Server':'nginx', 'Content-Type':f'{content_type}', 'Content-Length':f'{len(str_to_append.encode())}'}
+    cuerpo = str_to_append
+    cabezera["X-El-Que-Pregunta"] = parsed_msg[hpar.HEAD]["User-Agent"]
+    estrucutra_respuesta = {hpar.HEAD: cabezera, hpar.BODY:cuerpo}
+    http_response = hpar.to_http(estrucutra_respuesta)
+    print(http_response)
+    receiverSocket.send(http_response.encode())
     receiverSocket.close()
